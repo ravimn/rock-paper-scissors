af5f72091c65cc1ff2ba0eb7f9fa56f75d2f0c61
Ravi Namasivayam <ravimn@gmail.com>
2024-10-05 19:03:54 -0400

    Created game of rock/paper/scissors from scratch
    1. Created couple of functions for getting human and a randomized
       computer choice
    2. Compare choice and depending on who won increase the score of human
       or computer
    3. Play the game for 5 times and choose best of 5

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,62 @@
 console.log("Hello World")
-getComputerChoice();
+// getComputerChoice();
+// getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+const messageMap = new Map();
+messageMap.set('paperrock', "You Win! Paper beats Rock");
+messageMap.set('rockpaper', "You Lose! Paper beats Rock");
+messageMap.set('scissorspaper', "You Win! Scissors beats Paper");
+messageMap.set('paperscissors', "You Lose! Scissors beats Paper");
+messageMap.set('rockscissors', "You Win! Rock beats Scissors");
+messageMap.set('scissorsrock', "You Lose! Rock beats Scissors");
+
+const resultMap = new Map();
+resultMap.set('paperrock', 1);
+resultMap.set('rockpaper', 0);
+resultMap.set('scissorspaper', 1);
+resultMap.set('paperscissors', 0);
+resultMap.set('rockscissors', 1);
+resultMap.set('scissorsrock', 0);
+
+playGame();
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+    
+        playRound(humanSelection, computerSelection);
+        console.log( "Human Score is [" + humanScore + "] Computer Score is [" + computerScore + "]");     
+    }
+    console.log( "Final Score - Human [" + humanScore + "] Computer [" + computerScore + "]"); 
+
+}
+
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    console.log("Human Choice is " + humanChoice);
+    console.log("Computer Choice is " + computerChoice);
+
+    if (humanChoice === computerChoice) {
+        console.log( "You draw! Both played " + humanChoice );
+        return;
+    }
+
+    const play = humanChoice + computerChoice;
+    console.log("Play is " + play);
+    console.log(messageMap.get(play));
+
+    if (resultMap.get(play) === 1) {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
 
 function getComputerChoice() {
     /*
@@ -23,4 +80,17 @@ function getComputerChoice() {
 
     return symbol;
 
+}
+
+function getHumanChoice() {
+    while(true) {
+        let symbol = prompt("What is your choice - Rock, Paper or Scissors?");
+        symbol = symbol.toLowerCase();
+        console.log("Human selected choice is " + symbol);
+    
+        if (symbol === "rock" || symbol === "paper" || symbol === "scissors") {
+            return symbol;
+        }
+        console.log("Invalid choice.  Ask user to write the choice back");
+    }
 }
